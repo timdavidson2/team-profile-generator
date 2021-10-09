28df4c15c0c65b84f5f97718c6b02c9dc04735e3
Tim Davidson <timdavidson2@gmail.com>
2021-10-09 12:38:47 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+// set modules needed for app
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Manager = require("./lib/manager");
+const { createPromptModule } = require("inquirer");
