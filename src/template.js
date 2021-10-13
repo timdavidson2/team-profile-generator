44fd418d7672f47a828ce19b2ae8bd202f2226d9
Tim Davidson <timdavidson2@gmail.com>
2021-10-13 16:41:08 -0400

    Update template.js

--- src/template.js
+++ src/template.js
@@ -62,3 +62,57 @@ function genEngineer(obj) {
 	</div>
 	`;
 }
+
+function genIntern(obj) {
+  return `			
+<div class="col-2 cardCont">
+<div class="card" >
+<div class="card-body">
+<h5 class="card-title">${obj.getName()}</h5>
+<p class="card-text"><i class="bi bi-pencil-square"></i>${obj.getRole()}
+</p>
+</div>
+<ul class="list-group list-group-flush">
+<li class="list-group-item">ID: ${obj.getId()}</li>
+<li class="list-group-item">Email: <a href="mailto:${obj.getEmail()}">${obj.getEmail()}</a></li>
+<li class="list-group-item">School: ${obj.getSchool()}</li>
+</ul>
+</div>
+</div>
+`;
+}
+
+function genFile(team) {
+  return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link
+rel="stylesheet"
+href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+crossorigin="anonymous"
+/>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
+<link rel="stylesheet" href="./css/styles.css" />
+<title>Team</title>
+</head>
+<body>
+<header class='jumbotron'>
+<h1 class="display-4 text-center headerJumbo">My Team</h1>
+</header>
+<div class="row justify-content-center">
+${genManager(team)}
+</div>
+<div class="row justify-content-center">
+${genEmployees(team)}
+</div>
+</body>
+</html>
+`;
+}
+
+module.exports = genFile;
