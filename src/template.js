e4d1f52657ae21ffdd75116d298bc915b3145316
Tim Davidson <timdavidson2@gmail.com>
2021-10-13 16:38:23 -0400

    Update template.js

--- src/template.js
+++ src/template.js
@@ -16,3 +16,30 @@ function genEmployees(team) {
   });
   return htmlMain;
 }
+
+function genManager(team) {
+  let managerData = "";
+  team.forEach((obj) => {
+    if (obj.getRole() === "Manager") {
+      managerData = `			
+			<div class="col-2 cardCont">
+			<div class="card" >
+			<div class="card-body">
+			<h5 class="card-title">${obj.getName()}</h5>
+			<p class="card-text"><i class="bi bi-clipboard-check"></i>${obj.getRole()}
+			</p>
+			</div>
+			<ul class="list-group list-group-flush">
+			<li class="list-group-item">ID: ${obj.getId()}</li>
+			<li class="list-group-item">Email: <a href="mailto:${obj.getEmail()}">${obj.getEmail()}</a></li>
+			<li class="list-group-item">Office Number: ${obj.getOfficeNumber()}</li>
+			</ul>
+			</div>
+			</div>
+			`;
+    } else {
+      return "";
+    }
+  });
+  return managerData;
+}
